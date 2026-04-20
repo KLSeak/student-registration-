fe485ff846788dd73c5da2a81ed936789598a8b8
KLSeak <leangseakkong@gmail.com>
2026-04-20 20:02:51 +0700

    change axios

--- server.js
+++ server.js
@@ -10,8 +10,15 @@ const cors = require('cors')
 
 // middleware
 app.use(express.json())
-app.use(cors())
-
+app.use(cors({
+    origin: [
+      'https://student-regis-frontend.leangseakkong.workers.dev',
+      'http://localhost:5173'  // keep for local dev
+    ],
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+  }))
 // routes first ✅
 app.use('/api/auth', authRouter)
 app.use('/api/student', studentRouter)
