d30d23d4dc9296355dcf020b74cec55a99f59373
KLSeak <leangseakkong@gmail.com>
2026-04-20 20:29:06 +0700

    fix cloudinary storage

--- middleware/upload.middleware.js
+++ middleware/upload.middleware.js
@@ -1,30 +1,23 @@
 const multer = require('multer')
-const path = require('path')
-const fs = require('fs')
-const dayjs = require('dayjs')
-const {time} = require('console')
 const cloudinary = require('cloudinary').v2
 const { CloudinaryStorage } = require('multer-storage-cloudinary')
 
-const uploadPath = path.join(__dirname, '../upload')
-
-if(!fs.existsSync('upload')){
-    fs.mkdirSync('upload')
-}
-
+// Configure Cloudinary
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-  })
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+})
 
+// Use Cloudinary storage
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
-    folder: 'student-register',  // folder name in Cloudinary
+    folder: 'student-register',
     allowed_formats: ['jpg', 'jpeg', 'png', 'webp'],
   }
 })
 
-const upload = multer({storage})
+const upload = multer({ storage })
+
 module.exports = upload
